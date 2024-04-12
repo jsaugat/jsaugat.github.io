86c8a453a58a6175baea4c8b2e16aa181b7a5ac1
Mr. Saugat <jsaugatt02@gmail.com>
2024-04-12 12:57:53 +0545

    fix PreLoader

--- src/components/PreLoader.tsx
+++ src/components/PreLoader.tsx
@@ -1,19 +1,19 @@
 "use client"
 
-import React from "react";
+import React, { useRef } from "react";
 import { gsap } from "gsap";
 import { useGSAP } from "@gsap/react";
 
 function PreLoader({ numberOfBlinders = 6 }) {
-    //? useLayoutEffect = sync useEffect
+  const blinderContainer = useRef<HTMLDivElement>(null);
   useGSAP(() => {
     gsap.to(".blinder", {
       scaleY: 0,
       stagger: 0.1,
       duration: 0.8,
-      ease: "power3.out",
+      ease: "power4.out",
     });
-  }, []);
+  }, { scope: blinderContainer });
   
   //? syntax: Array.from( { length: X }, map function ). "_" is used as a convention for a parameter that is intentionally ignored
   const blinders = Array.from({ length: numberOfBlinders }, (_, index) => (
@@ -21,7 +21,7 @@ function PreLoader({ numberOfBlinders = 6 }) {
   ));
 
   return (
-    <div className="blinder-container absolute z-50 h-full w-full text-black flex gap-0 pointer-events-none">
+    <div ref={blinderContainer} className="blinder-container fixed z-50 h-full w-full text-black flex gap-0 pointer-events-none">
       {blinders}
     </div>
   );
